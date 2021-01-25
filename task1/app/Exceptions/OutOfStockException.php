0acadf3af457a21fa3cfe588daf38b5896bf8611
dodydestriady <dodydestriady@gmail.com>
2021-01-26 00:03:33 +0700

    add handler for out of stock product

---
+++ task1/app/Exceptions/OutOfStockException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class OutOfStockException extends Exception
+{
+	public function report()
+	{
+        \Log::debug('The product is out of stock');
+        //notification
+	}
+}
