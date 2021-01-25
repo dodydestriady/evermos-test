861d8e018d2dbe589f43a0a1784d2b49b1a08e8a
dodydestriady <dodydestriady@gmail.com>
2021-01-25 16:13:20 +0000

    add migration create products, sales, sale_details table

---
+++ task1/database/migrations/2021_01_22_133036_create_sale_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSaleDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sale_id');
+            $table->foreignId('product_id');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->timestamps();
+
+            $table->index('product_id');
+            $table->index('sale_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_details');
+    }
+}
